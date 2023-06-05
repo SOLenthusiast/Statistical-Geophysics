dcede7264b873725a2a02633062e747da3ffb6b5
Sol_enthusiast <101778567+SOLenthusiast@users.noreply.github.com>
2023-06-05 11:30:59 -0400

    Add files via upload

---
+++ Seismology_parkfield.py
@@ -0,0 +1,153 @@
+import numpy as np
+import pyplot.matplotlib as plt
+import seaborn as sns
+import pandas ad pd
+
+# Make the plot
+plt.plot(dcst.ecdf(mags), marker='.', linestyle='none')
+
+# Label axes and show plot
+plt.xlabel('magnitude')
+plt.ylabel('ECDF')
+plt.show()
+
+def b_value(mags, mt, perc=[2.5, 97.5], n_reps=None):
+    """Compute the b-value and optionally its confidence interval."""
+    # Extract magnitudes above completeness threshold: m
+    m = mags[mags >= mt]
+
+    # Compute b-value: b
+    b = (np.mean(m) - mt) * np.log(10)
+
+    # Draw bootstrap replicates
+    if n_reps is None:
+        return b
+    else:
+        m_bs_reps = dcst.draw_bs_reps(m, np.mean, size=n_reps)
+
+        # Compute b-value from replicates: b_bs_reps
+        b_bs_reps = (m_bs_reps - mt) * np.log(10)
+
+        # Compute confidence interval: conf_int
+        conf_int = np.percentile(b_bs_reps, perc)
+    
+        return b, conf_int
+
+
+# Compute b-value and 95% confidence interval
+b, conf_int = b_value(mags, mt, perc=[2.5, 97.5], n_reps=10000)
+
+# Generate samples to for theoretical ECDF
+m_theor = np.random.exponential(b/np.log(10), size=100000) + mt
+
+# Plot the theoretical CDF
+_ = plt.plot(*dcst.ecdf(m_theor))
+
+# Plot the ECDF (slicing mags >= mt)
+_ = plt.plot(*dcst.ecdf(mags[mags >= mt]), marker='.', linestyle='none')
+
+# Pretty up and show the plot
+_ = plt.xlabel('magnitude')
+_ = plt.ylabel('ECDF')
+_ = plt.xlim(2.8, 6.2)
+plt.show()
+
+# Report the results
+print("""
+b-value: {0:.2f}
+95% conf int: [{1:.2f}, {2:.2f}]""".format(b, *conf_int))
+
+# Compute the mean time gap: mean_time_gap
+mean_time_gap = np.mean(time_gap)
+
+# Standard deviation of the time gap: std_time_gap
+std_time_gap = np.std(time_gap)
+
+# Generate theoretical Exponential distribution of timings: time_gap_exp
+time_gap_exp = np.random.exponential(mean_time_gap, size=10000)
+
+# Generate theoretical Normal distribution of timings: time_gap_norm
+time_gap_norm = np.random.normal(mean_time_gap, std_time_gap, size=10000)
+
+# Plot theoretical CDFs
+_ = plt.plot(*dcst.ecdf(time_gap_exp))
+_ = plt.plot(*dcst.ecdf(time_gap_norm))
+
+# Plot Parkfield ECDF
+_ = plt.plot(*dcst.ecdf(time_gap, formal=True, min_x=-10, max_x=50))
+
+# Add legend
+_ = plt.legend(('Exp.', 'Norm.'), loc='upper left')
+
+# Label axes, set limits and show plot
+_ = plt.xlabel('time gap (years)')
+_ = plt.ylabel('ECDF')
+_ = plt.xlim(-10, 50)
+plt.show()
+
+# Draw samples from the Exponential distribution: exp_samples
+exp_samples = np.random.exponential(mean_time_gap, size=100000)
+
+# Draw samples from the Normal distribution: norm_samples
+norm_samples = np.random.normal(mean_time_gap, std_time_gap, size=100000)
+
+# No earthquake as of today, so only keep samples that are long enough
+exp_samples = exp_samples[exp_samples > today - last_quake]
+norm_samples = norm_samples[norm_samples > today - last_quake]
+
+# Compute the confidence intervals with medians
+conf_int_exp = np.percentile(exp_samples, [2.5, 50, 97.5]) + last_quake
+conf_int_norm = np.percentile(norm_samples, [2.5, 50, 97.5]) + last_quake
+
+# Print the results
+print('Exponential:', conf_int_exp)
+print('     Normal:', conf_int_norm)
+
+def ecdf_formal(x, data):
+    return np.searchsorted(np.sort(data), x, side='right') / len(data)
+
+def ks_stat(data1, data2):
+    # Compute ECDF from data: x, y
+    x, y = dcst.ecdf(data1)
+    
+    # Compute corresponding values of the target CDF
+    cdf = dcst.ecdf_formal(x, data2)
+
+    # Compute distances between concave corners and CDF
+    D_top = y - cdf
+
+    # Compute distance between convex corners and CDF
+    D_bottom = cdf - y + 1/len(data1)
+
+    return np.max((D_top, D_bottom))
+
+def draw_ks_reps(n, f, args=(), size=10000, n_reps=10000):
+    # Generate samples from target distribution
+    x_f = f(*args, size=size)
+    
+    # Initialize K-S replicates
+    reps = np.empty(n_reps)
+    
+    # Draw replicates
+    for i in range(n_reps):
+        # Draw samples for comparison
+        x_samp = f(*args, size=n)
+        
+        # Compute K-S statistic
+        reps[i] = dcst.ks_stat(x_samp, x_f)
+
+    return reps
+
+# Draw target distribution: x_f
+x_f = np.random.exponential(mean_time_gap, 10000)
+
+# Compute K-S stat: d
+d = dcst.ks_stat(x_f, time_gap)
+
+# Draw K-S replicates: reps
+reps = dcst.draw_ks_reps(len(time_gap), np.random.exponential, 
+                         args=(mean_time_gap,), size=10000, n_reps=10000)
+
+# Compute and print p-value
+p_val = np.sum(reps >= d) / 10000
+print('p =', p_val)
